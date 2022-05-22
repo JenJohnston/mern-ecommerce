b8b9686f7a46a90ae9a246e45439bab8300f636e
JenJohnston <73721221+JenJohnston@users.noreply.github.com>
2022-05-22 17:10:35 -0600

    .

---
+++ server/data.js
@@ -0,0 +1,61 @@
+const data = {
+    products: [
+        {
+            id: 1,
+            name: 'Nike Slim Shirt',
+            slug: 'nike-slim-shirt',
+            category: 'shirts',
+            image: '/images/p1.jpg',
+            price: 120,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.7,
+            numReviews: 10,
+            desc: 'high quality shirt'
+        },
+
+        {
+            id: 2,
+            name: 'Adidas Fit Shirt',
+            slug: 'adidas-fit-shirt',
+            category: 'shirts',
+            image: '/images/p2.jpg',
+            price: 250,
+            countInStock: 20,
+            brand: 'Puma',
+            rating: 4.3,
+            numReviews: 13,
+            desc: 'high quality shirt'
+        },
+
+        {
+            id: 3,
+            name: 'Nike Slim Pant',
+            slug: 'nike-slim-pant',
+            category: 'pants',
+            image: '/images/p3.jpg',
+            price: 70,
+            countInStock: 15,
+            brand: 'Nike',
+            rating: 3.8,
+            numReviews: 23,
+            desc: 'high quality item'
+        },
+
+        {
+            id: 4,
+            name: 'Adidas Fit Pant',
+            slug: 'adidas-fit-pant',
+            category: 'pants',
+            image: '/images/p4.jpg',
+            price: 95,
+            countInStock: 23,
+            brand: 'Nike',
+            rating: 4.2,
+            numReviews: 12,
+            desc: 'high quality item'
+        },
+    ],
+}
+
+export default data
