6485c391976701e8d4cdbf29060ce4eee497ef8d
JenJohnston <73721221+JenJohnston@users.noreply.github.com>
2022-05-23 10:29:08 -0600

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,10 @@
 import './scss/index.scss'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+import {LinkContainer} from 'react-router-bootstrap'
+
 import Product from './components/Product';
 import HomePage from './components/HomePage';
 
@@ -9,14 +13,25 @@ function App() {
     <Router>
         <div>
           <header>
-            <Link to="/">Amazona</Link>
+            <Navbar bg="dark" variant="dark">
+              <Container>
+                <LinkContainer to="/">
+                  <Navbar.Brand>Amazona</Navbar.Brand>
+                </LinkContainer>
+              </Container>
+            </Navbar>
           </header>
           <main>
-            <Routes>
-                <Route path="/" element={<HomePage/>}/>
-                <Route path="/product/:slug" element={<Product/>}/>
-            </Routes>
+            <Container>
+              <Routes>
+                  <Route path="/" element={<HomePage/>}/>
+                  <Route path="/product/:slug" element={<Product/>}/>
+              </Routes>
+            </Container>
           </main>
+          <footer>
+            <p className='text-center'>&copy; 2022 All Rights Reserved | For Demo Purposes Only</p>
+          </footer>
         </div>
     </Router>
   );
