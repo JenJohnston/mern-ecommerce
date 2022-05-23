c8d964d194d8811739a98f0b137b309fb5a5661b
JenJohnston <73721221+JenJohnston@users.noreply.github.com>
2022-05-23 17:15:08 -0600

    .

--- client/src/views/ProductPage.jsx
+++ client/src/views/ProductPage.jsx
@@ -44,7 +44,6 @@ export default function ProductPage() {
         error: ''
     })
 
-    // const [products, setProducts] = useState([])
 
     useEffect(() => {
         const fetchData = async () => {
@@ -57,15 +56,27 @@ export default function ProductPage() {
                 dispatch({type: 'FETCH_FAIL', payload: getError(err)})
             }
             
-            // setProducts(result.data)
+           
         }
         fetchData()
     }, [slug])
 
     const {state, dispatch: ctxDispatch} = useContext(Store)
+    const {cart} = state
 
-    const addToCartHandler = () => {
-        ctxDispatch({type: 'CART_ADD_ITEM', payload: {...product, quantity: 1}})
+    const addToCartHandler = async () => {
+        const existItem = cart.cartItems.find((x) => x._id === product._id)
+        const quantity = existItem ? existItem.quantity + 1 : 1
+        const { data } = await axios.get(`/api/products/${product._id}`)
+        if (data.countInStock < quantity) {
+            window.alert('Sorry. This product is out of stock')
+            return
+        }
+
+        ctxDispatch({
+            type: 'CART_ADD_ITEM', 
+            payload: {...product, quantity}
+        })
     }
 
     return (
