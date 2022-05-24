e2528339aca3ed7b8ecaeec7b783b260f2d4a08d
JenJohnston <73721221+JenJohnston@users.noreply.github.com>
2022-05-23 21:03:19 -0600

    .

--- server/routes/seedRoutes.js
+++ server/routes/seedRoutes.js
@@ -1,13 +1,16 @@
 import express from "express";
 import Product from "../models/productModel.js";
+import User from "../models/userModel.js";
 import data from "../data.js";
 
 const seedRouter = express.Router()
 
 seedRouter.get('/', async (req, res) => {
-    await Product.remove({})
-    const createProducts = await Product.insertMany(data.products)
-    res.send({createProducts}) 
+    await Product.remove({});
+  const createdProducts = await Product.insertMany(data.products);
+  await User.remove({});
+  const createdUsers = await User.insertMany(data.users);
+  res.send({ createdProducts, createdUsers });
 })
-
+ 
 export default seedRouter
